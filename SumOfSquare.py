b3e086d33f89e24b9d7ba4538fba2683475a67ef
Honelgn Yohannes <liranso392@gmail.com>
2025-05-03 13:33:50 -0700

    Create SumOfSquare.py

---
+++ SumOfSquare.py
@@ -0,0 +1,17 @@
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+       
+        l=0
+        r=int(c**0.5)
+        
+        while l<=r:
+            squar=((l**2) + (r**2))
+            
+            if squar==c:
+                return True
+                l+=1
+                r-=1
+            elif squar < c:
+                l+=1
+            else : r-=1
+        return False
