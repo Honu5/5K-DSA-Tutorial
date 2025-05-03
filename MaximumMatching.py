14e5c1edfb4da29a324b912c09d1bbdf9f5efd69
Honelgn Yohannes <liranso392@gmail.com>
2025-05-03 13:36:15 -0700

    Create MaximumMatching.py

---
+++ MaximumMatching.py
@@ -0,0 +1,35 @@
+class Solution:
+    def matchPlayersAndTrainers(self, players: List[int], trainers: List[int]) -> int:
+        f=0
+        s=0
+        players.sort()
+        trainers.sort()
+        n=max(len(players),len(trainers))
+        cnt=0
+        if len(players)<=len(trainers):
+            while n:
+                if players[f]<=trainers[s]:
+                    f+=1
+                    s+=1
+                    cnt+=1
+                    if cnt==min(len(players),len(trainers)):
+                        break
+                else: 
+                    s+=1
+                n-=1
+        else:
+            n=min(len(players),len(trainers))
+            while n:
+                if players[f]<=trainers[s]:
+                    f+=1
+                    s+=1
+                    cnt+=1
+                    n-=1
+                    if cnt==min(len(players),len(trainers)):
+                        break
+                
+                else: 
+                    s+=1           
+        return cnt
+
+            
